269de03ab2a2388c8f0206406141ebafbd827f98
ValentinStoyanov <56118614+ValentinStoyanov@users.noreply.github.com>
2020-10-15 09:13:17 +0200

    Add files via upload

---
+++ SIGET_Grupo_2/src/main/java/com/example/demo/SigetGrupo2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigetGrupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigetGrupo2Application.class, args);
+	}
+
+}

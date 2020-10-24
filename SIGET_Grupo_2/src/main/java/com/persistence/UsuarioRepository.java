9ff12726f173a54532a5f3128150dea01b82e1cc
Ismael Rabanal Martín <Ismael.Rabanal@alu.uclm.es>
2020-10-25 01:43:04 +0200

    Añadido método para buscar usuario por id

--- SIGET_Grupo_2/src/main/java/com/persistence/UsuarioRepository.java
+++ SIGET_Grupo_2/src/main/java/com/persistence/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package com.persistence;
 
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends MongoRepository<Usuario, String> {
+	Optional<Usuario> findOneBy_id(String id);
 }

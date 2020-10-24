0d8bd8cecffa4243adba99cfb6da4f2112dfe9a1
Ismael Rabanal Martín <Ismael.Rabanal@alu.uclm.es>
2020-10-25 01:10:35 +0200

    Tests para login añadidos

--- SIGET_Grupo_2/src/test/java/com/example/demo/PasosLogin.java
+++ SIGET_Grupo_2/src/test/java/com/example/demo/PasosLogin.java
@@ -1,17 +1,38 @@
 package com.example.demo;
 
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.spring.CucumberContextConfiguration;
 
 public class PasosLogin extends SigetGrupo2ApplicationTest{
 	
+	@Given("el usuario está registrado en la base de datos")
+	public void usuarioRegistrado() throws Throwable{
+	    /*Comprobar que el usuario con nombre name1 y contraseña pass1 está en la base de datos*/
+		System.out.println("Entra al Given");
+	}
+	
 	@When("el servidor recibe la llamada POST \\/login")
 	public void llamadaLogin() throws Throwable{
-	    System.out.println("Entra al when");
+		OkHttpClient client = new OkHttpClient();
+		MediaType mediaType = MediaType.parse("text/plain");
+		MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+		RequestBody body = RequestBody.create(JSON, "{}");
+		Request request = new Request.Builder()
+				.url("http://localhost:8080/login?username=name&password=pass1 ")
+				.method("POST", body)
+				.build();
+		Response response = client.newCall(request).execute();
+		System.out.println(response);
 	}
 	 
-	@Then("^el cliente recibe la respuesta de que el login es correcto$")
+	@Then("el cliente recibe la respuesta de que el login es correcto")
 	public void respuestaLogin() throws Throwable {
 		System.out.println("Entra al Then");
 	}
--- SIGET_Grupo_2/target/test-classes/features/login_test.feature
+++ SIGET_Grupo_2/target/test-classes/features/login_test.feature
@@ -1,6 +1,12 @@
 @tag
-Feature: Login can be performed
+Feature: Login funciona correctamente
 	@tag1
-	Scenario: El cliente hace la llamada POST /login a la API
+	Scenario Outline: El cliente hace la llamada POST /login a la API
+		Given el usuario está registrado en la base de datos
 		When el servidor recibe la llamada POST /login
 		Then el cliente recibe la respuesta de que el login es correcto
+	
+	Examples: 
+      | username  | password  | status  |
+      | name1     | pass1     | success |
+      | name2     | pass2     | Fail    |

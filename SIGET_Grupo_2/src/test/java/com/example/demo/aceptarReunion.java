0bd85e7176e783e87a1fe45fc1f1016efea125a0
africasimonsesmero <africasimonsesmero@localhost>
2020-10-29 12:16:19 +0100

    Test aceptar Reunones

---
+++ SIGET_Grupo_2/src/test/java/com/example/demo/aceptarReunion.java
@@ -0,0 +1,70 @@
+package com.example.demo;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class aceptarReunion extends SigetGrupo2ApplicationTest{
+	private OkHttpClient client;
+	private Request request;
+
+
+	
+	@Given("El usuario intenta aceptar la reunion")
+	public void el_usuario_intenta_aceptar_reunion() {
+	    System.out.println("El usuairo no ha aceptardo la reunion...");
+	}
+	@When("El usuario acepta la llamada POST temas {string}, descripcion {string}, hora_inicio {string}, hora_fin {string}, asistentes {string}, convocantes {string}")
+	public void el_usuairo_acepta_la_llamada_POST_con_los_parámetros_temas_descripcion_hora_inicio_hora_fin_asistentes_convocantes(String temas, String descripcion, String hora_inicio, String hora_fin, String asistentes, String convocantes) throws IOException {
+		client = new OkHttpClient();
+		MediaType mediaType = MediaType.parse("text/plain");
+		RequestBody body = RequestBody.create(mediaType, "");
+		request = new Request.Builder()
+		          .url("http://localhost:8080/reuniones/aceptarReunion?temas="+ temas + "&descripcion=" + descripcion + "&hora_inicio=" + hora_inicio + "&hora_fin=" + hora_fin + "&asistentes=" + asistentes + "&convocantes=" + convocantes)
+		          .post(body)
+		          .addHeader("Content-Type", "application/json")
+		          .addHeader("User-Agent", "PostmanRuntime/7.19.0")
+		          .addHeader("Accept", "*/*")
+		          .addHeader("Cache-Control", "no-cache")
+		          .addHeader("Postman-Token", "026c8d66-5ccb-453f-b1b4-c6f351f126ee,ca3db196-6148-4d81-a889-94d79002afe4")
+		          .addHeader("Accept-Encoding", "gzip, deflate")
+		          .addHeader("Content-Length", "84")
+		          .addHeader("Connection", "keep-alive")
+		          .addHeader("cache-control", "no-cache")
+		          .build();
+	}
+	@Then("el cliente recibe la respuesta de que el login es {string}")
+	public void el_usuario_recibe_la_respuesta_de_aceptar_la_reunion(String correcto) throws IOException, JSONException {
+		try {
+			Response response = client.newCall(request).execute();
+		    String body= response.body().string();
+			if(correcto.equals("True")) {
+		      if(body.equals("false")) {
+		        fail("La respuesta da error pero debería ser afirmativa");
+		      }
+		    }else{
+		      if(body.equals("true")) {
+		        fail("La respuesta es afirmativa pero debería dar error");
+		      }
+		    }
+		}catch(Exception e){
+			e.printStackTrace();
+		    fail("Error en la llamada http");
+		}
+	}
+}
+
+
---
+++ SIGET_Grupo_2/src/test/java/features/aceptarReunion_test.feature
@@ -0,0 +1,17 @@
+
+Feature: La reunion se ha aceptado correctamente
+
+  
+  Scenario Outline: El cliente llama al post de aceptar la reunion 
+    Given El usuario intenta aceptar la reunion
+    When El usuario acepta la llama al post para aceptar la reunion "<temas>", "<descripcion>", "<hora_inicio>", "<hora_fin>", "<asistentes>", "<convocante>"  
+  	Then El usuario recibe que la reunion ha sido aceptada "<correcto>" 
+
+    Examples: 
+      | Temas | Descripcion | Hora_Inicio | Hora_Fin  | Asistentes | Convocante  | Correcto |
+      | Tema1 |     Desc1   | HIni1       | HFin1     | Asist1     | Conv1       | True     |
+      | Tema2 |     Desc2   | HIni2       | HFin2     | Asist2     | Conv2       | True     |
+      | Tema3 |     Desc3   | HIni3       | HFin3     | Asist3     | Conv3       | True     |    
+      | Tema4 |     Desc4   | HIni4       | HFin4     | Asist4     | Conv4       | False    |
+      | Tema5 |     Desc5   | HIni5       | HFin5     | Asist5     | Conv5       | False 	  |
+      | Tema6 |     Desc6   | HIni6       | HFin6     | Asist6     | Conv6       | False    |

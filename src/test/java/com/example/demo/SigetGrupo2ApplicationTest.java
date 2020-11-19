ed666facaf6cf8a693da477493d69333294a61f4
ValentinStoyanov <56118614+ValentinStoyanov@users.noreply.github.com>
2020-11-19 15:14:03 +0100

    Sonar coverage implemented

--- src/test/java/com/example/demo/SigetGrupo2ApplicationTest.java
+++ src/test/java/com/example/demo/SigetGrupo2ApplicationTest.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import io.cucumber.spring.CucumberContextConfiguration;
@@ -7,5 +8,12 @@ import io.cucumber.spring.CucumberContextConfiguration;
 @CucumberContextConfiguration
 @SpringBootTest
 public class SigetGrupo2ApplicationTest {
+	
+	@Test
+	public void helloWorld() {
+		System.out.println("Hola");
+	}
 
 }
+
+
